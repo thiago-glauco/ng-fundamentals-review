6cfa07cf59f6c26b512208cbcd6a715be09c57c6
thiago-glauco <thiagoglauco@yahoo.com.br>
2022-07-23 05:58:22 -0300

    1 file modified

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,11 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Hello Angular</h1>',
+  template: `
+  <h1>Hello Angular</h1>
+  
+  <a href="https://pixabay.com/users/innovalabs-22783312/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6521720">Innova Labs</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6521720">Pixabay</a>
+  `,
 })
 export class EventsAppComponent  {
   name = 'Angular ' + VERSION.major;
